a21ca24c3e4d02e898117f5765d1337fa9cf0827
Lucas <troyamanda7@gmail.com>
2024-05-30 21:13:25 -0300

    Ultimos cambios

--- assets/Js/nuevo.js
+++ assets/Js/nuevo.js
@@ -1,40 +1,3 @@
-var options = {
-  accessibility: true,
-  prevNextButtons: true,
-  pageDots: true,
-  setGallerySize: false,
-  arrowShape: {
-    x0: 10,
-    x1: 60,
-    y1: 50,
-    x2: 60,
-    y2: 45,
-    x3: 15,
-  },
-};
-
-var carousel = document.querySelector("[data-carousel]");
-var slides = document.getElementsByClassName("carousel-cell");
-var flkty = new Flickity(carousel, options);
-
-flkty.on("scroll", function () {
-  flkty.slides.forEach(function (slide, i) {
-    var image = slides[i];
-    var x = ((slide.target + flkty.x) * -1) / 3;
-    image.style.backgroundPosition = x + "px center";
-  });
-});
-
-// let prevScrollpos = window.scrollY;
-// window.onscroll = function() {
-//   let currentScrollPos = window.scrollY;
-//   if (currentScrollPos > prevScrollpos) {
-//     document.getElementById("navbar").style.position = "fixed";
-//   } else {
-//     document.getElementById("navbar").classList.add = ("animate__animated animate__fadeIn");
-//     document.getElementById("navbar").style.position = "static";
-
-//   }};
 
 window.addEventListener("scroll", function () {
   const menu = document.querySelector("#navbar");
@@ -51,8 +14,14 @@ window.addEventListener("scroll", function () {
 toggle between hiding and showing the dropdown content */
 function dropdown() {
   document.getElementById("myDropdown").classList.toggle("show");
+ var elemento = document.getElementById("enlace").innerHTML;
+ var cambio = elemento.replace("&#8593;","&#8595;");
+ document.getElementById("enlace").innerHTML = cambio;
 }
-
+function dropdown2() {
+  const miCheckbox = document.getElementById('menu-toggle');
+  miCheckbox.checked = false;
+}
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
   if (!event.target.matches('#enlace')) {
@@ -82,3 +51,11 @@ document.addEventListener("DOMContentLoaded", (event) => {
   // Use a single event listener, instead of a function that runs on every scroll event
   window.addEventListener("resize", adjustLink);
 });
+$(function () {
+  $('#vidBox').VideoPopUp({ 
+   	backgroundColor: "#17212a",
+   	opener: "video1",
+    maxweight: "640",
+    idvideo: "v1"
+  });
+ });
